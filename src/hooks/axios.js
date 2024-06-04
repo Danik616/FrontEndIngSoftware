2e2f0f7dad65f8fc750b2d6beb20ea1ce6c1d2cc
Alejandro León <juan.aleon2001@gmail.com>
2024-06-04 14:08:33 -0500

    chore: Add withCredentials option to axios instance

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -4,8 +4,7 @@ const api = axios.create({
   baseURL: 'http://api.draxterindustry.com/api',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
-  withCredentials: true
+  }
 });
 
 export default api;
